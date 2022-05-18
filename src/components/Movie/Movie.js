e7701f0152416abbda328aa18b3a11315bafa50f
Nayara <nrdeandrade@gmail.com>
2022-05-18 17:50:48 -0300

    feat: escolha de sessão a partir da id do filme e exibição das informações de sessões em layout

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -1,5 +1,89 @@
-export default function Movie () {
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+import axios from "axios";
+
+import "../../assets/reset.css"
+import "./style.css"
+
+
+function Showtime ( {showtime, index} ) {
     return (
-        <h1>Sou o filme</h1>
+        <div className="button">
+            {showtime}
+        </div>
+
+    )
+}
+
+function MovieSection ( {date, id, showtimes, weekday} ) {
+    console.log(showtimes)
+
+    return (
+        <section>
+            <span>
+                    {weekday} - {date}
+            </span>
+            <div className="flex">
+                {
+                    showtimes.map((showtime, index) => (
+                        <Showtime
+                        key={index} 
+                        showtime={showtime.name}
+                        index={index}
+                        />                                           
+                    ))
+                }               
+                
+            </div>
+        </section>
+    )
+
+}
+
+
+
+export default function Movie () {
+    const [sections, setSections] = useState([]);
+    const { idfilme } = useParams();
+
+    useEffect(() => {
+        const promise = axios.get(`https://mock-api.driven.com.br/api/v5/cineflex/movies/${idfilme}/showtimes`);
+
+        promise.then((response) => {
+            setSections(response.data.days);
+            console.log(response.data.days)
+            
+        });
+    }, [])
+
+
+    return (
+        <>
+        <header>
+            <h1>CINEFLEX</h1>
+        </header>
+        <main>
+            <div className="title center">
+                <h2>Selecione o horário</h2> 
+            </div>
+            {
+                sections.map((section, index) => (
+                    <MovieSection
+                    key={index}
+                    date={section.date}
+                    id={section.id}
+                    showtimes={section.showtimes}
+                    weekday={section.weekday} 
+                    /> 
+                ))
+            }
+            
+
+        </main>
+        
+        
+        
+        </>
+        
     )
 }
